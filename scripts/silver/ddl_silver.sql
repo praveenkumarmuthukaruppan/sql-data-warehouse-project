db559db89770c64942d73240222de0fda345ec56
praveenkumarmuthukaruppan <135541676+praveenkumarmuthukaruppan@users.noreply.github.com>
2026-01-04 13:42:41 +0000

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 if OBJECT_ID('silver.crm_cust_info','U') is not null
     drop table silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
